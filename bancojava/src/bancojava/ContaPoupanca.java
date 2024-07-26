2a730f097787a97604fabce7742a7c25e3383725
Luiz Fabio <luizbinho26@gmail.com>
2024-07-26 13:33:25 -0300

    Ad Atualizacoes dos exercicios propostos em Aula

--- bancojava/src/bancojava/ContaPoupanca.java
+++ bancojava/src/bancojava/ContaPoupanca.java
@@ -1,6 +1,11 @@
 package bancojava;
 
 public class ContaPoupanca extends Conta {
+	
+	public ContaPoupanca(Cliente cliente) {
+		super(cliente);
+	}
+
 	@Override
 	public void imprimirExtratro() {
        System.out.println("=== Extrato Conta Poupança ===");
